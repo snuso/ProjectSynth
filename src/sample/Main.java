f191e8e3dfe32b1e90ff8e0da445a00815f0c164
Jakob <dyffen60@gmail.com>
2020-10-15 16:38:29 +0200

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,44 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    static String filePath;
+
+    @Override
+    public void start(Stage primaryStage) {
+
+
+        primaryStage.setTitle("JavaFX App");
+
+        FileChooser fileChooser = new FileChooser();
+
+        Button button = new Button("Select File");
+        button.setOnAction(e -> {
+            File selectedFile = fileChooser.showOpenDialog(primaryStage);
+        });
+
+        fileChooser.setTitle("Open Resource File");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Audio Files", "*.wav", "*.mp3", "*.aac"));
+        VBox vBox = new VBox(button);
+        Scene scene = new Scene(vBox, 960, 600);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        filePath = fileChooser.getInitialFileName();
+        System.out.println(filePath);
+    }
+}
